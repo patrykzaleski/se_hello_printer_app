107fb351b6d0ed09585520bc0321670a3b73f148
patrykzaleski <patrykzaleski@users.noreply.github.com>
2019-02-03 14:07:00 +0100

    zzz

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,7 +4,7 @@ from formater import SUPPORTED, PLAIN
 from flask import request
 
 moje_imie = "Patryk"
-msg = "Hello World!"
+msg = "Witaj Swiecie!"
 
 
 @app.route('/')
--- test/test_views.py
+++ test/test_views.py
@@ -14,4 +14,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Patryk", "mgs":Witaj Swiecie!"}', rv.data)
+        self.assertEquals('{ "imie":"Patryk", "mgs":"Witaj Swiecie!"}', rv.data)
